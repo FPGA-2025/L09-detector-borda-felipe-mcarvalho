df7a65152bb1d8d3423871c6e34f4842fc9a732f
Felipe Carvalho <felipemarinho92020@gmail.com>
2025-07-10 20:36:35 -0300

    adicionando detector-borda.v

--- detector-borda.v
+++ detector-borda.v
@@ -2,9 +2,20 @@ module detector_borda (
     input clk,
     input rst,
     input [1:0] entrada,
-    output [1:0] detector
+    output reg [1:0] detector
 );
     
 //insira seu código aqui
+    reg [1:0] entrada_anterior;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            entrada_anterior <= 2'b00;
+            detector <= 2'b00;
+        end else begin
+            detector <= entrada & ~entrada_anterior;
+            entrada_anterior <= entrada;
+        end
+    end
 
 endmodule
